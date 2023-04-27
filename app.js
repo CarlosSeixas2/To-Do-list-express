56e64b35f42bff5e27b179529ea39e151f308254
Seixas <caduphb@gmail.com>
2023-04-27 14:32:24 -0300

    New-Routes

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Minha Lista de Tarefas</h1>')
+});
+
+app.get('/json', (req, res) => {
+    res.json({
+        name: "Houston",
+        task: "Arrumar a Casa",
+        done: false
+    });
+})
+
+app.listen(5000, () =>{
+    console.log("Server Iniciado!")
+});
