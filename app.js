e9008887c42352cf330dc23024a0d345bc5c1936
Seixas <caduphb@gmail.com>
2023-05-15 07:54:53 -0300

    To-Do-List#02

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const path = require('path');
 
 const checklistRouter = require('./src/routes/checklist');
 const taskRouter = require('./src/routes/task');
-
-
 const rootRouter = require('./src/routes/index');
+
 const methodOverride = require('method-override');
 
 //Conectando com o Banco de Dados
 require('./config/database');
 
-
 const app = express();
 app.use(express.json());
+
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
 
